cbec8bb55661bb9cb2ec8cde354f22ec76701acf
Bastian <55935198+BM-Master@users.noreply.github.com>
2023-03-16 10:55:05 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: './',
+   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 
   transpileDependencies: true,
   // accept pdf files
