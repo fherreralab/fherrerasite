dfa689700c3b263e4a80044d0f2c99e5a2c6481b
Felipe Herrera <faherreraur@gmail.com>
2025-12-27 22:32:18 -0300

    updated news text

--- src/pages/group_news/news/congrats/FelipeAppointedAdjointFacultyWaterloo.vue
+++ src/pages/group_news/news/congrats/FelipeAppointedAdjointFacultyWaterloo.vue
@@ -5,7 +5,7 @@
             
 
              <p class="my-3">
-                Felipe accepted a secondary appointment as Adjunt Faculty at the Department of Mechanical & Mechatronics Engineering to develop collaborations 
+                Felipe accepted a fixed-term secondary appointment as Adjunt Faculty at the Department of Mechanical & Mechatronics Engineering to develop collaborations 
                 and student mentoring in the Advanced Quantum Materials Engineering, with focus on metal-organic frameworks materials for quantum optoelectroncis and spin qubits, 
                 for applications in quantum technology.
             </p>
